3e3cfa07fd682ed91216725a4c66b36e2781936d
claseudem <164520182+claseudem@users.noreply.github.com>
2026-02-07 01:20:50 +0000

    primera clase

---
+++ main.py
@@ -0,0 +1,11 @@
+# Ejercicio 1 (Fácil): Variables y Operaciones Básicas
+# Define dos variables: `capital_inicial` (1000) y `tasa_interes` (0.05).
+# Calcula el `interes_simple` al final del año.
+# Imprime el resultado.
+
+capital_inicial = 1000
+tasa_interes = 0.05
+
+interes_simple = capital_inicial * tasa_interes
+
+print("Interés simple al final del año:", interes_simple)
